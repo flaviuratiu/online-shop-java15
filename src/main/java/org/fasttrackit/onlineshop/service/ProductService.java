2c7b0b9347bc16111cf502b52fe78107418f34a7
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-06-22 21:30:57 +0300

    added update and delete product methods

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -6,6 +6,7 @@ import org.fasttrackit.onlineshop.persistence.ProductRepository;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -49,5 +50,20 @@ public class ProductService {
         return productRepository.findById(id)
                 // lambda expression
                 .orElseThrow(() -> new ResourceNotFoundException("Product " + id + " not found."));
+    }
+
+    public Product updateProduct(long id, SaveProductRequest request ) {
+        LOGGER.info("Updating product {}: {}", id, request);
+
+        Product product = getProduct(id);
+
+        BeanUtils.copyProperties(request, product);
+
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(long id) {
+        LOGGER.info("Deleting product {}", id);
+        productRepository.deleteById(id);
     }
 }
--- src/test/java/org/fasttrackit/onlineshop/ProductServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshop/ProductServiceIntegrationTests.java
@@ -60,6 +60,34 @@ class ProductServiceIntegrationTests {
                 () -> productService.getProduct(0));
     }
 
+    @Test
+    void updateProduct_whenValidRequest_thenReturnUpdatedProduct() {
+        Product product = createProduct();
+
+        SaveProductRequest request = new SaveProductRequest();
+        request.setName(product.getName() + " Updated");
+        request.setPrice(product.getPrice() + 10);
+        request.setQuantity(product.getQuantity() + 10);
+
+        Product updatedProduct = productService.updateProduct(product.getId(), request);
+
+        assertThat(updatedProduct, notNullValue());
+        assertThat(updatedProduct.getId(), is(product.getId()));
+        assertThat(updatedProduct.getName(), is(request.getName()));
+        assertThat(updatedProduct.getPrice(), is(request.getPrice()));
+        assertThat(updatedProduct.getQuantity(), is(request.getQuantity()));
+    }
+
+    @Test
+    void deleteProduct_whenExistingProduct_thenProductDoesNotExistAnymore() {
+        Product product = createProduct();
+
+        productService.deleteProduct(product.getId());
+
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> productService.getProduct(product.getId()));
+    }
+
     private Product createProduct() {
         SaveProductRequest request = new SaveProductRequest();
         request.setName("Phone");
