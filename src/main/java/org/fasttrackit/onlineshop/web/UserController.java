f8e907d0a8f6eb616f37491394fe81708cc921b1
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-07-13 18:48:51 +0300

    added user controller

---
+++ src/main/java/org/fasttrackit/onlineshop/web/UserController.java
@@ -0,0 +1,34 @@
+package org.fasttrackit.onlineshop.web;
+
+import org.fasttrackit.onlineshop.domain.User;
+import org.fasttrackit.onlineshop.service.UserService;
+import org.fasttrackit.onlineshop.transfer.user.CreateUserRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(CreateUserRequest request) {
+        User user = userService.createUser(request);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable long id) {
+        User user = userService.getUser(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
